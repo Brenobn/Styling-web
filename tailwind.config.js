0e17f8c85cf847f7c185c2f508aa0c1b03c916a6
Brenobn <brenobragabn@gmail.com>
2025-08-27 16:39:39 -0300

    Setting gridTemplateColumns in the tailwind.config.js file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/**@type {import {'tailwindcss'}.Config} */
+/** @type {import {'tailwindcss'}.Config} */
 module.exports = {
-  content: [
-    './src/**/*.tsx',
-  ],
+  content: ['./src/**/*.tsx'],
   theme: {
     extend: {
+      gridTemplateColumns: {
+        app: '250px, 1fr',
+      },
       colors: {
         breno: '#8257e6',
-      }
+      },
     },
   },
   plugins: [],
-  
 }
