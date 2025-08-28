c2e38257812306f738df169f3f751cdd124f0647
Brenobn <brenobragabn@gmail.com>
2025-08-28 10:25:28 -0300

    Adjusting Tailwind v4 syntax and adjusting imports for the globals.css file. Updated configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,13 @@
-/** @type {import {'tailwindcss'}.Config} */
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.tsx'],
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  // O theme fica vazio ou pode até ser removido, pois a customização agora está no CSS.
   theme: {
-    extend: {
-      gridTemplateColumns: {
-        app: '250px, 1fr',
-      },
-      colors: {
-        breno: '#8257e6',
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
