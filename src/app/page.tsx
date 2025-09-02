75821029f2ba77f1c073bfcee4aa9ab3a37be990
Brenobn <brenobragabn@gmail.com>
2025-09-02 11:42:28 -0300

    Importing Select and SelectItem and starting to use them

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,8 @@ import { SettingsTabs } from '@/components/SettingsTabs'
 import * as Input from '@/components/Input'
 import * as FileInput from '@/components/Form/FileInput'
 import { Mail } from 'lucide-react'
+import { Select } from '@/components/Form/Select'
+import { SelectItem } from '@/components/Form/Select/SelectItem'
 
 export default function Home() {
   return (
@@ -110,7 +112,10 @@ export default function Home() {
             >
               Country
             </label>
-            <div></div>
+            <Select placeholder="Select a country">
+              <SelectItem value="br" text="Brazil" />
+              <SelectItem value="us" text="United States" />
+            </Select>
           </div>
 
           <div className="grid-cols-form grid gap-3 pb-5">
@@ -120,7 +125,15 @@ export default function Home() {
             >
               Timezone
             </label>
-            <div></div>
+            <div>
+              <Select placeholder="Select a timezone">
+                <SelectItem
+                  value="utc8"
+                  text="Pacific Standard Time (UTC-08:00)"
+                />
+                <SelectItem value="utc3" text="America São Paulo (UTC-03:00)" />
+              </Select>
+            </div>
           </div>
 
           <div className="grid-cols-form grid gap-3 pb-5">
